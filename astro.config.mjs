843bb09da21f17e8c0a5ffad061f10dfb9f0806d
Odd Egil Hegge Selnes <post@odde.org>
2024-12-27 13:21:37 +0100

    set up working sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: "9chq8hlo",
+      dataset: "production",
+      useCdn: true,
+    }),
+    react(),
+  ],
+});
