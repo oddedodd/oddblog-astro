2cb68f50c60f60f32f804ce5ba5ad914f7db5c85
Odd Egil Hegge Selnes <post@odde.org>
2025-01-12 00:22:09 +0100

    added tailwind with basic configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,13 @@ import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "9chq8hlo",
-      dataset: "production",
-      useCdn: true,
-    }),
-    react(),
-  ],
+  integrations: [sanity({
+    projectId: "9chq8hlo",
+    dataset: "production",
+    useCdn: true,
+  }), react(), tailwind()],
 });
